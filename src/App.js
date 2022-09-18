b7ea258cdeaef74d4441ac5030fa055ff367dcce
ANIRBAN CHAKRABORTY <anirban.chakraborty@cuemath.com>
2022-09-19 01:52:40 +0530

    lint fix

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { localStorageGet, localStorageSet } from "./utils";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { v4 as uuidv4 } from "uuid";
 import Home from "./pages/home";
 
 function App() {
-  const dummyRoute = {
-    name: "199",
-    direction: "UP",
-    id: `107_${uuidv4()}`,
-    status: "ACTIVE",
-    stops: [
-      {
-        lat: 12.9795865148043,
-        lng: 77.5911622741734,
-        id: "stop1",
-      },
-      {
-        lat: 12.9801301594259,
-        lng: 77.5919776656823,
-        id: "stop2",
-      },
-      {
-        lat: 12.9784155838887,
-        lng: 77.5912481048586,
-        id: "stop3",
-      },
-    ],
-  };
+  const dummyRoute = useMemo(
+    () => ({
+      name: "199",
+      direction: "UP",
+      id: `107_${uuidv4()}`,
+      status: "ACTIVE",
+      stops: [
+        {
+          lat: 12.9795865148043,
+          lng: 77.5911622741734,
+          id: "stop1",
+        },
+        {
+          lat: 12.9801301594259,
+          lng: 77.5919776656823,
+          id: "stop2",
+        },
+        {
+          lat: 12.9784155838887,
+          lng: 77.5912481048586,
+          id: "stop3",
+        },
+      ],
+    }),
+    []
+  );
 
   useEffect(() => {
     if (!localStorageGet()) {
       localStorageSet(dummyRoute);
     }
-  }, []);
+  }, [dummyRoute]);
 
   return <Home initData={dummyRoute} />;
 }
